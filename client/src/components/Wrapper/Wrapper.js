c7f8cf83d34dd401a8aa9c0de39c66dc31c2d4d0
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2023-05-24 10:59:58 +0200

    Added Wrapper component

---
+++ client/src/components/Wrapper/Wrapper.js
@@ -0,0 +1,11 @@
+const Wrapper = () => {
+  return (
+    <div className="wrapper">
+      <header>To jest menu</header>
+      <main>To jest sekcja główna</main>
+      <footer>To jest stopka</footer>
+    </div>
+  );
+};
+
+export default Wrapper;
